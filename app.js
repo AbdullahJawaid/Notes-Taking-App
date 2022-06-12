288665232bc41a6bac14464cd5fcd1514411e6c6
AbdullahJawaid <abdullahjawaid30@gmail.com>
2022-06-13 04:13:58 +0500

    complete Notes Taking App

--- app.js
+++ app.js
@@ -62,3 +62,90 @@ function showNotes(){
 
 // fuction declear
 showNotes();
+// work or edit state
+function showMenu(elem) {
+    // console.log(elem.parentElement);
+    elem.parentElement.classList.add('show');
+    document.addEventListener('click', e =>{
+        // removing show class from setting menu on document click
+        if (e.target.tagName !='I' || e.target != elem) {
+            elem.parentElement.classList.remove('show')
+            
+        }
+    })
+    
+}
+
+function deleteNote(noteId) {
+
+    let confirmDel=confirm('Are You Sure?')
+    if (!confirmDel) return;
+        
+    
+
+
+    //removing selected note from array
+    notes.splice(noteId,1)
+
+    //saving update note to localStroge
+    localStorage.setItem('notes',JSON.stringify(notes));
+    showNotes();
+}
+
+function UpdateNote(noteId,title,desc){
+    isUpdate = true;
+    updateId =noteId;
+    addBox.click();
+    titleTag.value=title;
+    descTag.value=desc;
+    addBtn.innerText="Update Note";
+    popupTitle.innerText="Update a Note";
+    // console.log(noteId,title,desc)
+}
+
+addBtn.addEventListener('click',e =>{
+    e.preventDefault();
+
+    //Get addNewNote Text and Des values
+    let noteTitle=titleTag.value,
+        noteDecs=descTag.value;
+
+
+    if(noteTitle || noteDecs){
+
+        //getting month ,day,year, from the current date
+        let dateObj=new Date(),
+            month=months[dateObj.getMonth()],
+            day=dateObj.getDay(),
+            year=dateObj.getFullYear();
+
+
+
+    
+        let noteInfo={
+            title:noteTitle,
+            description:noteDecs,
+            date:`${month} ${day} ${year}`
+        }
+
+        if (!isUpdate) {
+         //Adding new note to new notes
+             notes.push(noteInfo);
+
+            
+        }else{
+        //updating specified note
+            isUpdate = false;
+            notes[updateId]= noteInfo;
+
+        }
+
+        // saving in localStroge 
+        localStorage.setItem('notes',JSON.stringify(notes));
+        closeIcon.click();
+        showNotes();
+    }
+
+    
+})
+    
